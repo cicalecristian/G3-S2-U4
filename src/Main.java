64e263188eea4fc337b72fefb7e6934a7cad868c
cicalecristian <cicalecristian@gmail.com>
2026-03-18 15:50:07 +0100

    first ex.

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,18 @@ public class Main {
         products.add(new Product("Don Chisciotte", 738189, 20.0, "books"));
         products.add(new Product("I promessi sposi", 639286, 110.0, "books"));
         products.add(new Product("Guerra e pace", 925142, 120.0, "books"));
+        products.add(new Product("salviette", 172933, 1.50, "baby"));
+        products.add(new Product("ciuccio", 283746, 3.0, "baby"));
+        products.add(new Product("pannolini", 998152, 5.0, "baby"));
+        products.add(new Product("t-shirt", 638928, 15.0, "boys"));
+        products.add(new Product("air-max", 192833, 110.0, "boys"));
+        products.add(new Product("occhiali da sole", 524221, 20.0, "boys"));
         products.add(new Product("Xiaomi", 936254, 1.300, "smartphone"));
         products.add(new Product("Realme", 172936, 1.100, "smartphone"));
+        products.add(new Product("Samsung", 732648, 1.500, "smartphone"));
 
-        
+        List<Product> filteredBooks = products.stream().filter(book -> book.getCategory().equals("books") && book.getPrice() > 100).toList();
+        filteredBooks.forEach(System.out::println);
+
     }
 }
